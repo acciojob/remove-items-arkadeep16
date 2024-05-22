d4efdd0f18111176607af635af5c74529341e3e9
arkadeep16 <75775078+arkadeep16@users.noreply.github.com>
2024-05-23 01:58:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const colorSelect = document.getElementById('colorSelect');
+const btn = document.querySelector('input');
+console.log(btn);
+
+function removeItems() {
+  let colors = colorSelect.value;
+  // console.log(colors);
+  const selectedOption =
+    colorSelect.options[colorSelect.selectedIndex].remove();
+}
+btn.addEventListener('click', removeItems);
